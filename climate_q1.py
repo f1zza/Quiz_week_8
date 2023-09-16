ce49cd119bc18a3dcb535f8a9367d450b75dc134
f1zza <fizza.batool@griffithuni.edu.au>
2023-09-16 14:22:08 +1000

    commit one

--- climate_q1.py
+++ climate_q1.py
@@ -1,9 +1,19 @@
 import matplotlib.pyplot as plt
+import climate.db
         
 years = []
 co2 = []
 temp = []
 
+conn = climate.db.connect('climate.db')
+cursor = conn.cursor()
+cursor.execute("SELECT years, co2, temp FROM ClimateData")
+for row in cursor.fetchall():
+    years.append(row[0])
+    co2.append(row[1])
+    temp.append([2])
+conn.close()
+    
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
